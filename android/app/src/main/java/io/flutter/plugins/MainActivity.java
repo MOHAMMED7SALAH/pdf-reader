8071e87fcbbb84d7d7939a52f2320a37fac5d3d1
MOHAMMED7SALAH <bentitimohammedsalahstars7@gmail.com>
2022-04-29 23:16:00 +0200

    pdf-app

---
+++ android/app/src/main/java/io/flutter/plugins/MainActivity.java
@@ -0,0 +1,9 @@
+import io.flutter.plugin.common.MethodChannel;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  
+ }
